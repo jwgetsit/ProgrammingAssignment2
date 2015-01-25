77c0f7dc1e83f9c3a75037fc3951231dc2b18d08
jwgetsit <jyotikawadhwa@gmail.com>
2015-01-24 18:01:49 -0800

    Programming Assignment 2
    Caching the inverse matrix operation

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+#The two functions below allow caching of potentially time-consuming computation of calculating the inverse.
 
-## Write a short comment describing this function
+#makeCacheMatrix creates a special "vector", which is a list containing a function to
+#set the value of the matrix
+#get the value of the matrix
+#set the value of the inverse
+#get the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+   
+        m <- NULL
+        setmat <- function(y) {
+            my_matrix <<- y
+            m <<- NULL
+        }
+        getmat <- function() my_matrix
+        setinverse_mat <- function(inverse_mat) m <<- inverse_mat
+        getinverse_mat <- function() m
+        list(setmat = setmat, getmat = getmat,
+        setinverse_mat = setinverse_mat,
+        getinverse_mat = getinverse_mat)
+    
 
 }
 
+# cacheSolvecalculates the inverse of the special "vector" created with the above function. However, it first checks to see if the inverse has already been calculated and that the matrix has not changed. If so, it gets the inverse from the cache and skips the computation. Otherwise, it calculates the inverse of the data and sets the value of the matrix and the inverse in the cache.
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, current_matrix = matrix(), ...) {
+    
+    data <- x$getmat()
+    m <- x$getinverse_mat()
+    if (missing(current_matrix)) {
+        current_matrix <- data
+    }
+    if(!is.null(m) && identical(data, current_matrix)) {
+        message("getting cached data")
+        return(m)
+    }
+    
+    m <- solve(current_matrix, ...)
+    x$setmat(current_matrix)
+    x$setinverse_mat(m)
+    m # Return a matrix that is the inverse of 'current_matrix'
+    
 }
